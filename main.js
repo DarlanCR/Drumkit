95c36dd56c0fd32e1ee84392f53ab58f536ec443
Darlan Cristian <darlancristian36@gmail.com>
2022-06-27 10:14:02 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    autoHideMenuBar: true,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
